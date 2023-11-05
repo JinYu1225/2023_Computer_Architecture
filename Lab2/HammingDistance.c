d3522e03f9c5c95826d1b971038dcc8ea09cf469
JinYu1225 <zcxvedenlin@gmail.com>
2023-11-05 22:05:48 +0800

    Updates the optimization files of HammingDistance

--- Lab2/HammingDistance.c
+++ Lab2/HammingDistance.c
@@ -1,20 +1,35 @@
 // This code is modify from yptang5488.
 // Try to implement CLZ through out the whole hamming distance code.
-
-# ifndef __GNUC__
-# define __asm__ asm
-# endif
-
 #include <stdio.h>
+#include <inttypes.h>
 
 typedef unsigned long long uint64_t;
 typedef unsigned short uint16_t;
+typedef uint64_t ticks;
+
 uint64_t test1_x0 = 0x0000000000100000;
 uint64_t test1_x1 = 0x00000000000FFFFF;
 uint64_t test2_x0 = 0x0000000000000001;
 uint64_t test2_x1 = 0x7FFFFFFFFFFFFFFE;
 uint64_t test3_x0 = 0x000000028370228F;
 uint64_t test3_x1 = 0x000000028370228F;
+
+static inline ticks getticks(void)
+{
+    uint64_t result;
+    uint32_t l, h, h2;
+    asm volatile(
+        "rdcycleh %0\n"
+        "rdcycle %1\n"
+        "rdcycleh %2\n"
+        "sub %0, %0, %2\n"
+        "seqz %0, %0\n"
+        "sub %0, zero, %0\n"
+        "and %1, %1, %0\n"
+        : "=r"(h), "=r"(l), "=r"(h2));
+    result = (((uint64_t) h) << 32) | ((uint64_t) l);
+    return result;
+}
 
 uint16_t count_leading_zeros(uint64_t x) {
     x |= (x >> 1);
@@ -39,6 +54,7 @@ int HammingDistance(uint64_t x1, uint64_t x2) {
     int Hdist = 0;
     uint64_t c1 = x1 ^ x2;
     uint16_t clz_count = 0;
+
     // stop when there is no different between x1 and x2
     while(c1 != 0) {
         // find biggest different bit by clz
@@ -49,20 +65,17 @@ int HammingDistance(uint64_t x1, uint64_t x2) {
     return Hdist;
 }
 
-unsigned long get_cycle_count(void) {
-    unsigned long cycle;
-    asm volatile ("rdcycle %0" : "=r" (cycle));
-    return cycle;
-}
+// unsigned long get_cycle_count(void) {
+//     unsigned long cycle;
+//     asm volatile ("rdcycle %0" : "=r" (cycle));
+//     return cycle;
+// }
 
 int main() {
-    unsigned long start_cycle, end_cycle;
-
-    start_cycle = get_cycle_count();
-    HammingDistance(test1_x0, test1_x1);
-    end_cycle = get_cycle_count();
-    printf("Cycle Count: %lu\n", end_cycle - start_cycle);
-    // printf("Hamming Distance = %d\n", HammingDistance(test1_x0, test1_x1));
+    ticks t0 = getticks();
+    printf("Hamming Distance = %d\n", HammingDistance(test1_x0, test1_x1));
+    ticks t1 = getticks();
+    printf("Cycle Count: %llu\n", t1 - t0);
     // printf("Hamming Distance = %d\n", HammingDistance(test2_x0, test2_x1));
     // printf("Hamming Distance = %d\n", HammingDistance(test3_x0, test3_x1));
     return 0;
